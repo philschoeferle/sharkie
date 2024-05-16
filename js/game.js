a32e9ec6ac2ab3835699e6821cb6c2c5d9603e80
philschoeferle <philipp.schoeferle@web.de>
2024-05-16 17:24:54 +0200

    start-screen first commit

--- js/game.js
+++ js/game.js
@@ -55,6 +55,36 @@ window.addEventListener("keyup", (e) => {
   }
 });
 
+function toggleVolume() {
+  let volumeImg = document.getElementById("volume-img");
+
+  if (volumeImg.src.includes("volume-off.png")) {
+    volumeImg.src = "img/6.Botones/Menu/volume-on.png";
+  } else {
+    volumeImg.src = "img/6.Botones/Menu/volume-off.png";
+  }
+}
+
+function toggleInstructions() {
+  let instructionsImg = document.getElementById("instructions-img");
+
+  if (instructionsImg.src.includes("instruction-open.png")) {
+    instructionsImg.src = "img/6.Botones/Menu/instruction-close.png";
+  } else {
+    instructionsImg.src = "img/6.Botones/Menu/instruction-open.png";
+  }
+}
+
+function toggleFullscreen() {
+  let fullscreenImg = document.getElementById("fullscreen-img");
+
+  if (fullscreenImg.src.includes("fullscreen-open.png")) {
+    fullscreenImg.src = "img/6.Botones/Menu/fullscreen-close.png";
+  } else {
+    fullscreenImg.src = "img/6.Botones/Menu/fullscreen-open.png";
+  }
+}
+
 function clearAllIntervals() {
   let highestTimeoutId = setInterval(";");
   for (var i = 0; i < highestTimeoutId; i++) {
